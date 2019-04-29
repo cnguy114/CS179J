6f98fbc46891c96d144bce92a6fd6d37644c711d
Brittney Mun <Y0ungMun@users.noreply.github.com>
2019-04-28 20:51:36 -0700

    Motors run, but logic not working.

--- Sorting Arm/main.c
+++ Sorting Arm/main.c
@@ -1,5 +1,5 @@
 /*
- * Sorting Arm main.c file.
+ * Sorting Arm.c
  * This is the code for the sorting arm portion of the recycling sorter project.
  * This code controls the stepper motors.
  */ 
@@ -24,7 +24,7 @@ unsigned char button;
 unsigned int motor1;
 unsigned int motor2;
 int i1, i2;						//iterators for array
-int stepArr = [A, AB, B, BC, C, CD, D, DA];
+int stepArr[] = {A, AB, B, BC, C, CD, D, DA};
 
 void MotorTick(){
 	switch(motorState){
@@ -79,38 +79,77 @@ void MotorTick(){
 	}
 	switch(motorState){
 		case Start:
-			break;
-		case Neutral:
-			motor1 = stepArr[i1];				//Start at A for both motors
+			motor1 = stepArr[i1];
 			motor2 = stepArr[i2];
 			break;
-		case Left:
+		case Neutral:
+			motor1 = stepArr[0];				//Start at A for both motors
+			motor2 = stepArr[0];
+			PINA = (motor1 << 4) | motor2;
+			break;
+		case Left:								//Both spin right
 			if (i1 < sizeof(stepArr))						//As long as either motor is not at the end of the array, increment by 1.
-				i1 +=1;								//otherwise, set back to the start of the array.
+				i1 += 1;								//otherwise, set back to the start of the array.
 			else
 				i1 = 0;
-			//Both spin right
+			if (i2 < sizeof(stepArr))
+				i2 += 1;
+			else
+				i2 = 0;
+			motor1 = stepArr[i1];
+			motor2 = stepArr[i2];
+			PINA = (motor1 << 4) | motor2;			//Send byte to motors. One nibble = 1 motor
 			break;
-		case Mid:
-			//One motor spins left, other spins right.
+		case Mid:								//One motor spins left, other spins right.
+			if (i1 > 0)							//Still unsure of direction. As long as both start and end on different values, it's fine.
+				i1 -= 1;
+			else
+				i1 = sizeof(stepArr);
+			if (i2 < sizeof(stepArr))
+				i2 += 1;
+			else
+				i2 = 0;
+			motor1 = i1;
+			motor2 = i2;
+			PINA = (motor1 << 4) | motor2;
 			break;
-		case Right:
-			//Both spin left
+		case Right:								//Both spin left
+			if (i1 > 0)
+				i1 -= 0;
+			else
+				i1 = sizeof(stepArr);
+			if (i2 > 0)
+				i2 -= 1;
+			else
+				i2 = sizeof(stepArr);
+			motor1 = i1;
+			motor2 = i2;
+			PINA = (motor1 << 4) | motor2;
 			break;
 		default:
 			break;
 	}
 }
 
+// 4/27 MOTORS WORK FINALLY AHAHAA
+// 4/28 Button logic isn't working. Motors are both vibrating, unsure of direction.
 int main(void){
-	DDRA = 0x00; PORTA = 0xFF;
-	DDRB = 0xFF; PORTB = 0x00;
-	TimerSet(300);					//Lab manual suggests 3ms between ticks
+	DDRA = 0xFF; PORTA = 0x00;		//PortA is output.
+	DDRB = 0x00; PORTB = 0xFF;		//PortB is input.
+	TimerSet(2);					//Need small timer or motors won't spin. Maybe not enough power going through?
 	TimerOn();
 	
-	
     while (1) {
-		MotorTick();
+		button = ~PINB;				//Enable pin
+		MotorTick();				//Tick function
+/*		int i = 0;
+		while (i < sizeof(stepArr)){
+			PINA = (stepArr[i] << 4) | stepArr[i]; 
+			i++;
+			if (i >= sizeof(stepArr))
+				i = 0;
+		}
+	*/	
 		while(!TimerFlag);
 		TimerFlag = 0;
     }
