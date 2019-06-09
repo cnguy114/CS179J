380396eefe08f36b6e2c35db9020f956ef75e971
cnguy114 <cnguy114@ucr.edu>
2019-06-09 15:28:52 -0700

    demo integration

--- botDetect.py
+++ botDetect.py
@@ -1,47 +1,45 @@
 from gpiozero import Button
 from gpiozero import LED
-import time
+from time import sleep
 import camCap
 #this checks a a gpio from the arduino. If the pin goes high, there is an object on the staging area
 
-photoRes = Button(26,hold_time=0.5)
+photoRes = Button(26)
 sig0 = LED(20)
 sig1 = LED(21)
 ready = LED(16)
-state = 0
+state = 3
 #check for arduino signal
 ready.on()
 def sendMsg(c):
-	if c == 0:
+	if c == 3:
 		sig0.off()
 		sig1.off()
-	elif c == 1:
+	elif c == 0:
 		sig0.on()
 		sig1.off()
-	elif c == 2:
+	elif c == 1:
 		sig0.off()
 		sig1.on()
-	elif c == 3:
+	elif c == 2:
 		sig0.on()
 		sig1.on()
 	else:
 		print("ERROR: Invalid Classification")
-	photoRes.wait_for_release()
+	sleep(3)
 
 def imgCap():
     return camCap.takePic()
 
 def changeState():
-    if photoRes.is_held:
+    if photoRes.is_pressed:
         return imgCap()
     else:
-        return 0
+        return 3
 
 while True:
+    print ("Changing State")
     state = changeState()
     
-    #print ("state: " + str(state))
+    print ("state: " + str(state))
     sendMsg(state)
-
-    time.sleep(1)
-    state = 0
